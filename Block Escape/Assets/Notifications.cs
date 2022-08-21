eeacd20ead136c504bc673d54e27ea0985e28cc6
Denno Louis <dennolouis@13.176.20.172.s.wireless.uri.edu>
2022-08-20 23:07:44 -0400

    notifications work for IOS

--- Block Escape/Assets/Notifications.cs
+++ Block Escape/Assets/Notifications.cs
@@ -1,12 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_ANDROID
 using Unity.Notifications.Android;
+#elif UNITY_IOS
+using Unity.Notifications.iOS;
+#endif
 using UnityEngine;
 
 public class Notifications : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+    {
+#if UNITY_ANDROID
+        AndroidNotification();
+#elif UNITY_IOS
+        IOSNotification();
+#endif
+
+    }
+
+    IEnumerator RequestAuthorization()
+    {
+        var authorizationOption = AuthorizationOption.Alert | AuthorizationOption.Badge;
+        using (var req = new AuthorizationRequest(authorizationOption, true))
+        {
+            while (!req.IsFinished)
+            {
+                yield return null;
+            };
+
+            string res = "\n RequestAuthorization:";
+            res += "\n finished: " + req.IsFinished;
+            res += "\n granted :  " + req.Granted;
+            res += "\n error:  " + req.Error;
+            res += "\n deviceToken:  " + req.DeviceToken;
+            Debug.Log(res);
+        }
+    }
+
+    void IOSNotification()
+    {
+        StartCoroutine(RequestAuthorization());
+
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            TimeInterval = new TimeSpan(6, 0, 0),
+            Repeats = false
+        };
+
+        var notification = new iOSNotification()
+        {
+            // You can specify a custom identifier which can be used to manage the notification later.
+            // If you don't provide one, a unique string will be generated automatically.
+            Identifier = "_notification_01",
+            Title = "Hey! Come Back!",
+            Body = "Can You Beat Your High Score?",
+            Subtitle = "See Where You Rank On The Leaderboard!",
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "category_a",
+            ThreadIdentifier = "thread1",
+            Trigger = timeTrigger,
+        };
+
+        iOSNotificationCenter.ScheduleNotification(notification);
+    }
+#if UNITY_ANDROID
+    void AndroidNotification()
     {
         //remove any notifications already displayed
         AndroidNotificationCenter.CancelAllDisplayedNotifications();
@@ -31,13 +93,12 @@ public class Notifications : MonoBehaviour
         var id = AndroidNotificationCenter.SendNotification(notification, "channel_id");
 
         //reschedule notification
-        if(AndroidNotificationCenter.CheckScheduledNotificationStatus(id) == NotificationStatus.Scheduled)
+        if (AndroidNotificationCenter.CheckScheduledNotificationStatus(id) == NotificationStatus.Scheduled)
         {
             AndroidNotificationCenter.CancelAllNotifications();
             AndroidNotificationCenter.SendNotification(notification, "channel_id");
         }
-
     }
 
-     
+#endif 
 }
